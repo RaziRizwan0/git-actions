e1540e9e0a46d93efe7d876d731aff3c86596668
razirizwan <razirizwan0908@gmail.com>
2025-07-14 16:37:05 +0530

    new

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
+<<<<<<< HEAD
   res.send('woow its made a huge  change');
+=======
+  res.send('woow test its change');
+>>>>>>> 15453ce (new)
 });
 
 app.listen(port, () => {
